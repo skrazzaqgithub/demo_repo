7dbcc58c319ed65d67a21a8699ef5a72fa02dab6
skrazzaqgithub <skrazzaq2727@gmail.com>
2025-07-30 12:26:40 +0530

    committed

--- tests/test_addition.py
+++ tests/test_addition.py
@@ -1,3 +1,9 @@
 def addition(a, b):
     return a + b
 print(addition(5, 3))  # Output: 8
+def test_addition():
+    assert addition(2, 3) == 5
+    assert addition(-1, 1) == 0
+    assert addition(0, 0) == 0
+    assert addition(100, 200) == 300
+    assert addition(-5, -5) == -10
