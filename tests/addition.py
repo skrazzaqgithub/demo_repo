b110070a6c08261c14d67416b80827dde9dca224
skrazzaqgithub <skrazzaq2727@gmail.com>
2025-07-30 11:28:03 +0530

    testcases are added and committed

---
+++ tests/addition.py
@@ -0,0 +1,3 @@
+def addition(a, b):
+    return a + b
+print(addition(5, 3))  # Output: 8
---
+++ tests/multiply.py
@@ -0,0 +1,4 @@
+def multily(a, b):
+    """Multiplies two numbers."""
+    return a * b
+print(multily(3, 4))  # Example usage, should print 12
